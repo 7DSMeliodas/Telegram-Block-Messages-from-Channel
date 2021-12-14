8206e127ebb270dc6e6974e5f4524a6ac371229b
Meliodas <navycis@me.com>
2021-12-14 04:16:55 +0100

    Update main.py
    added diffrent log types

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 from pyrogram import Client, filters
 
-app = Client("BlockMsgFromChannel", "API_ID", "API_Hash")
+app = Client("my_session", "API_ID", "API_Hash")
 
 LOG_GROUP = -9876
 GROUPS_TO_PROTECT = [-1234, -4321]
-LOG = true
+LOG = "nice"
 
-@app.on_message(filters.chat(GROUPS_TO_PROTECT))
+
+@app.on_message(filters.chat(GROUPS_TO_PROTECT)
 def check_for_user(Client, msg):
     if msg.sender_chat:
         msg.delete()
-        if log:
-          app.send_message(LOG_GROUP, "I deleted the following message:")
-          app.send_message(LOG_GROUP, msg)
-         else:
-          pass
+        if LOG == "max":
+            app.send_message(LOG_GROUP, "I deleted the following message:")
+            app.send_message(LOG_GROUP, msg)
+        elif LOG == "nice":
+            if len(msg.text) < 4062:
+                app.send_message(LOG_GROUP, "I deleted the following message:\n{}".format(msg.text))
+            else:
+                app.send_message(LOG_GROUP, "I deleted the following message:")
+                app.send_message(LOG_GROUP, msg.text)
+            app.send_message(LOG_GROUP,
+                             "The message was from this channel:\nChannelname: {}\nChannellink: @{}\n".format(
+                                 msg.sender_chat.title, msg.sender_chat.username))
+            app.send_message(LOG_GROUP, "The message was in this group:\n{}".format(msg.chat.title))
+        else:
+            pass
+
     else:
         pass
 
